e6ff4f581078e2a7e71d3db91cab3652931f153c
Ammodit <ammodit@yandex.ru>
2020-11-26 16:38:39 +0300

    added photo in ui abd fixed js

--- dist/static/js/index.js
+++ dist/static/js/index.js
@@ -1,23 +1,14 @@
-const objFromForm =() => {
-
-    const elements = document.forms[0].elements;
-    const obj = {}
-  
-    for (let i = 0; i < elements.length; i++) {
-      obj[elements[i].name] = elements[i].value;
-    }
-  
-  console.log(obj);
-  }
-
-const test = () => {
-    alert("ehf");
+let form = document.forms[0];
+    form.onsubmit = function(evt) {
+   evt.preventDefault();
+    objFromForm();
 }
 
-
-
- // let form = document.forms[0];
-    // let name = form.elements.name;
-    // for (let key in name) {
-    //   console.log(key)
-    // }
+function objFromForm() {
+    const elements = document.forms[0].elements;
+    const obj = {}  
+    for (let i = 0; i < elements.length; i++) {
+      obj[elements[i].name] = elements[i].value;
+    } 
+    console.log(obj);
+  }
