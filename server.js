f5629b26cb4e9b3d5f9fcc907050fdc3264bfe27
Ammodit <ammodit@yandex.ru>
2021-02-19 22:44:37 +0300

    fixed server and added new regexp

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express=require('express');
+const express = require("express");
+const path = require("path");
 
-const app=express();
-const PORT=4000;
+const app = express();
+const PORT = 4000;
 
-app.use(express.static('./dist'));
+app.use(express.static("./dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
-}); 
+});
--- src/data/valData.ts
+++ src/data/valData.ts
@@ -1,6 +1,4 @@
 export const valdata: {} = {
-    "tel": new RegExp("/^\\+?(\\d{1,3})?[- .]?\\(?(?:\\d{2,3})\\)?[- .]?\\d\\d\\d[- .]?\\d\\d\\d\\d/$/"),
-    "email": new RegExp("/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i"),
-    "login": new RegExp('[\p{L}\p{N}\p{P}\p{S}\p{Z}]'),
-    "password": new RegExp('[\p{L}\p{N}\p{P}\p{S}\p{Z}]')
+    "login": /[^\p{L}\p{N}\p{P}\p{S}\p{Z}]/,
+    "password": /[^\p{L}\p{N}\p{P}\p{S}\p{Z}]/
 }
