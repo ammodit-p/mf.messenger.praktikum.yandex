ce691b44aa6447222c1e4d44b13804a449554128
Ammodit <ammodit@yandex.ru>
2021-02-27 11:14:44 +0300

    made some tests for block

--- test/router.spec.ts
+++ test/router.spec.ts
@@ -6,13 +6,25 @@ import { expect } from "chai";
 import { router } from "../src/initialaze";
 
 
-describe ('router', function() {
+describe ('router check chat', function() {
 
     before(function () {
         router.go('/chat')
     })
     it('should go to chat', function(){
         expect(global.location.pathname).to.be.equal('/chat')
+    })
+
+})
+
+
+describe ('router check 404', function() {
+
+    before(function () {
+        router.go('/wrong_page')
+    })
+    it('should go to 404', function(){
+        expect(global.location.pathname).to.be.equal('/404')
     })
 
 })
