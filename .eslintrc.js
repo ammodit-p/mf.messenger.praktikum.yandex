3744d86f3e72091b2012ae7cb219de16f52b6ee1
Ammodit <ammodit@yandex.ru>
2021-03-06 17:48:19 +0300

    allowed any type for aguements

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,7 +34,8 @@ module.exports = {
         "rules": {
           "@typescript-eslint/no-inferrable-types": "off",
           "@typescript-eslint/no-explicit-any": "off",
-          "@typescript-eslint/no-empty-function": "off"
+          "@typescript-eslint/no-empty-function": "off",
+          "@typescript-eslint/explicit-module-boundary-types": ['error', { "allowArgumentsExplicitlyTypedAsAny": true }]
         }
     }
   ]
