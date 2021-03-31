1a86031527812c811a784adab26be580c7bc60df
Ammodit <ammodit@yandex.ru>
2021-03-31 09:15:05 +0300

    add user, delete user, add chat, delete chat

--- src/pages/chats/chats_events.ts
+++ src/pages/chats/chats_events.ts
@@ -35,7 +35,12 @@ export const 	events = {
 		if (event.target.getAttribute('data-show') === 'add-user') {
 			const popup: any = document.getElementById('add-user');
 			popup.classList.add('show');
-        }
+		}
+
+		if (event.target.getAttribute('data-show') === 'delete-user') {
+			const popup: any = document.getElementById('delete-user');
+			popup.classList.add('show');
+		}
 
 
     },
@@ -63,6 +68,7 @@ export const 	events = {
             if(checked === false) {return};
 
             const formData = new FormData(form);
+
             chat_controller.deleteUser(formData);
             event.target.classList.remove('show');
 		}
