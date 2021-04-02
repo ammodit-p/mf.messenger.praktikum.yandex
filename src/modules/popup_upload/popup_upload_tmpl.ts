49cb27ce38f086a2cfc36beadd30d945dab742b5
Ammodit <ammodit@yandex.ru>
2021-04-02 19:22:41 +0300

    fixed margins in popups

--- src/modules/popup_upload/popup_upload_tmpl.ts
+++ src/modules/popup_upload/popup_upload_tmpl.ts
@@ -1,7 +1,7 @@
 export const popup_upload_tmpl: string =
 `
 <div class="popup_upload {{form_class}}">
-<h1 class="popup_header {{form_header_class}}">{{content}}</h1>
+<h1 class="popup_form_header {{form_header_class}}">{{content}}</h1>
 
 	<label class="popup_upload_label">
 		Выбрать файл на компьютере
